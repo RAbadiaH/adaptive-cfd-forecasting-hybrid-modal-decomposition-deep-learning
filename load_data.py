55c4d328b29502b8465b83cc0dacd2a5f85bfebb
RAbadiaH <128459831+RAbadiaH@users.noreply.github.com>
2025-04-30 16:56:50 +0200

    Add remaining scripts

---
+++ load_data.py
@@ -0,0 +1,34 @@
+import scipy.io
+import mat73
+
+import numpy as np
+
+def load_data(case):
+
+    print("\nLoading Data ...", end='\r')
+    
+    if (case == 'cil3D'):
+        f = mat73.loadmat('./Datasets/laminar_flow.mat')
+        data_ten = f['Tensor']
+        data_ten = data_ten[..., 100:]
+
+    elif (case == 'JetLES'):
+        f = scipy.io.loadmat('./Datasets/jetLES.mat')
+        data_ten = f.get('p')
+        data_ten = np.transpose(data_ten, [1,2,0])
+        data_ten = data_ten[None, ..., :2000]
+    
+    elif (case == 'vki'):
+        f = mat73.loadmat('./Datasets/turbulent_flow.mat')
+        data_ten = f.get('Tensor')
+        data_ten = data_ten[..., :2000]
+
+    else:
+
+        raise TypeError("Dataset not found, check load_data.py.")
+
+    print("Loading Data: DONE\n")
+
+    print("\n")
+
+    return data_ten
